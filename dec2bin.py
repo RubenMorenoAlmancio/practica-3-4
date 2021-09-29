ab820f6427717e1e27ebd88ca4ba29858cc4df11
albaperezz <al415381@uji.es>
2021-09-29 16:21:38 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -30,3 +30,4 @@ if __name__ == "__main__":
     print("El numero " + str(numero_decimal) + " es " + numero_binario + " en binario con " + str(numero_bits) + " bits.")
 
     #Modificación para practica 3
+#modificacion
